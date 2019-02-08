7266916ab5375d0fcff9ba10391ceff32039b3c6
mikeevans85 <baudecordier@gmail.com>
2019-02-07 20:35:56 -0600

    Added sliding rows to train screen

--- screens/TrainScreen.js
+++ screens/TrainScreen.js
@@ -1,54 +1,56 @@
 import React, { Component } from 'react';
 import { ListView } from 'react-native';
-import { Badge, Container, Header, Content, Icon, List, ListItem, Text } from 'native-base';
+import { Badge, Button, Container, Header, Content, Icon, List, ListItem, Text } from 'native-base';
+
+const datas = [
+'Red Line',
+'Blue Line',
+'Green Line',
+'Brown Line',
+'Orange Line',
+'Yellow Line',
+'Purple Line',
+'Pink Line',
+];
+
 export default class TrainScreen extends Component {
+    constructor(props) {
+    super(props);
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      basic: true,
+      listViewData: datas,
+    };
+  }
+  deleteRow(secId, rowId, rowMap) {
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    const newData = [...this.state.listViewData];
+    newData.splice(rowId, 1);
+    this.setState({ listViewData: newData });
+  }
   render() {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     return (
       <Container>
         <Header />
         <Content>
-          <List>
-            <ListItem>
-            <Badge>
-              <Text>Red Line</Text>
-            </Badge>
-            </ListItem>
-            <ListItem>
-            <Badge primary>
-              <Text>Blue Line</Text>
-            </Badge>
-            </ListItem>
-            <ListItem>
-            <Badge success>
-              <Text>Green Line</Text>
-            </Badge>
-            </ListItem>
-            <ListItem>
-            <Badge danger>
-              <Text>Brown Line</Text>
-            </Badge>
-            </ListItem>
-            <ListItem>
-            <Badge warning>
-              <Text>Orange Line</Text>
-            </Badge>
-            </ListItem>
-            <ListItem>
-            <Badge Yellow>
-              <Text>Yellow Line</Text>
-            </Badge>
-            </ListItem>
-            <ListItem>
-            <Badge success>
-              <Text>Purple Line</Text>
-            </Badge>
-            </ListItem>
-            <ListItem>
-            <Badge success>
-              <Text>Pink Line</Text>
-            </Badge>
-            </ListItem>
-          </List>
+            <List
+            leftOpenValue={75}
+            rightOpenValue={-75}
+            dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+            renderRow={data =>
+              <ListItem>
+                <Text> {data} </Text>
+              </ListItem>}
+            renderLeftHiddenRow={data =>
+              <Button full onPress={() => alert(data)}>
+                <Icon active name="information-circle" />
+              </Button>}
+            renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+              <Button full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
+                <Icon active name="trash" />
+              </Button>}
+          />
         </Content>
       </Container>
     );
