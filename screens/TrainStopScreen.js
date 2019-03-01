f2689f2c5c0e1df483bd09d82bae09f0c28be164
mikeevans85 <baudecordier@gmail.com>
2019-02-28 18:28:45 -0600

    Changed layout of Train Screen page

--- screens/TrainStopScreen.js
+++ screens/TrainStopScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Text } from "native-base";
-import { View } from "react-native";
+import { Container, Header, Content, Card, CardItem, Text, Body  } from "native-base";
+import { StyleSheet, View } from "react-native";
 
 export default class extends Component {
 	constructor(props) {
@@ -12,9 +12,41 @@ export default class extends Component {
     const name = this.props.navigation.getParam('train', true);
     const latitude = this.props.navigation.getParam('latitude', true);
     return (
-      <View>
-        <Text>{name} {latitude}</Text>
-      </View>
+    //   <View>
+    //     <Text style={styles.titleText}>{name}</Text>
+    //     <Text style={styles.baseText}>{latitude}</Text>
+    //   </View>
+    // );
+    <Container>
+        <Header />
+        <Content padder>
+          <Card>
+            <CardItem header button onPress={() => alert("This is the #{name}")}>
+              <Text style={styles.titleText}>{name}</Text>
+            </CardItem>
+            <CardItem button onPress={() => alert("This is Card Body")}>
+              <Body>
+                <Text>
+                  Click on any carditem
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer button onPress={() => alert("This is Card Footer")}>
+              <Text>GeekyAnts</Text>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
